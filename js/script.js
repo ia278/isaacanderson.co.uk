704dea3f6c93afbf81122da710d15a85b3e5bfec
Isaac Anderson <isaacaanderson4@gmail.com>
2018-07-04 19:03:12 +0100

    commit v3 of site

--- js/script.js
+++ js/script.js
@@ -1,41 +1,28 @@
 // Constants
-const navB = document.querySelector(".nav-b");
-const navBItemContainer = document.querySelector(".nav-b-item-container");
+const hamburger = document.querySelector(".hamburger");
+const options = document.querySelector(".header-options");
 
 // Functions
 const actionOnWindowResize = () => {
 	const windowWidth = window.innerWidth;
-	if (windowWidth >= 577) {
-		navBItemContainer.style.display = "block";
+	if (windowWidth >= 668) {
+		options.style.display = "block";
 		return;
 	}
-	navBItemContainer.style.display = "none";
-	navB.classList.remove("change");
+	options.style.display = "none";
+	hamburger.classList.remove("change");
 };
 const animateHamburger = () => {
-	navB.classList.toggle("change");
-	if (navB.classList.contains("change")) {
-		navBItemContainer.style.display = "block";
+	hamburger.classList.toggle("change");
+	if (hamburger.classList.contains("change")) {
+		options.style.display = "block";
 		return;
 	}
-	navBItemContainer.style.display = "none";
+	options.style.display = "none";
 	actionOnWindowResize();
 };
-const dateForCopyright = () => {
-	const date = new Date();
-	const year = date.getFullYear();
-	const navC = document.querySelector(".nav-c");
-	const toRemove = document.querySelector("#copyright");
-	const p = document.createElement("P");
-	const sentence = `&copy isaac anderson 2017 - ${year}`;
-	p.innerHTML = sentence;
-	navC.removeChild(toRemove);
-	navC.appendChild(p);
-};
+
 
 // Event listeners
-navB.addEventListener("click", animateHamburger);
+hamburger.addEventListener("click", animateHamburger);
 window.addEventListener("resize", actionOnWindowResize);
-
-// Run functions
-dateForCopyright();
